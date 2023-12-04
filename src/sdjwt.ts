819364f11353d4f43d3648d2e870b51732870f9c
Lukas.J.Han <lukas.j.han@gmail.com>
2023-12-04 23:43:39 +0900

    Add interfaces
    Signed-off-by: Lukas.J.Han <lukas.j.han@gmail.com>

--- src/sdjwt.ts
+++ src/sdjwt.ts
@@ -6,10 +6,12 @@ import { KBJwt, kbHeader, kbPayload } from './kbjwt';
 export class SDJwt<
   Header extends Record<string, any> = Record<string, any>,
   Payload extends Record<string, any> = Record<string, any>,
+  KBHeader extends kbHeader = kbHeader,
+  KBPayload extends kbPayload = kbPayload,
 > {
-  public jwt: any;
-  public disclosures: Array<any> = [];
-  public keyBinding?: any;
+  public jwt?: Jwt<Header, Payload>;
+  public disclosures?: Array<Disclosure<any>>;
+  public keyBinding?: KBJwt<KBHeader, KBPayload>;
 
   public static decodeSDJwt<
     Header extends Record<string, any> = Record<string, any>,
@@ -45,4 +47,32 @@ export class SDJwt<
       kbJwt,
     };
   }
+
+  public static fromEncode<
+    Header extends Record<string, any> = Record<string, any>,
+    Payload extends Record<string, any> = Record<string, any>,
+    KBHeader extends kbHeader = kbHeader,
+    KBPayload extends kbPayload = kbPayload,
+  >(encodedSdJwt: string): SDJwt<Header, Payload> {
+    const { jwt, disclosures, kbJwt } = SDJwt.decodeSDJwt<
+      Header,
+      Payload,
+      KBHeader,
+      KBPayload
+    >(encodedSdJwt);
+
+    return new SDJwt<Header, Payload, KBHeader, KBPayload>(
+      jwt,
+      disclosures,
+      kbJwt,
+    );
+  }
+
+  public issue() {}
+
+  public present() {}
+
+  public verify() {}
+
+  public encodeSDJwt() {}
 }
