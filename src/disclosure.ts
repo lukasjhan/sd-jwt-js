035edf8e18088e87104fe78f6dfa30b44529e46f
Lukas.J.Han <lukas.j.han@gmail.com>
2023-12-04 23:11:00 +0900

    Add disclosure
    Signed-off-by: Lukas.J.Han <lukas.j.han@gmail.com>

--- src/disclosure.ts
+++ src/disclosure.ts
@@ -1,15 +1,16 @@
 import { Base64Url } from './base64url';
 import { SDJWTException } from './error';
+import { Hasher } from './type';
 
-export type DisclosureData = [string, string, unknown] | [string, unknown];
+export type DisclosureData<T> = [string, string, T] | [string, T];
 
-export class Disclosure {
+export class Disclosure<T> {
   private salt: string;
   private key?: string;
-  private value: unknown;
+  private value: T;
   private _digest: string | undefined;
 
-  public constructor(data: DisclosureData) {
+  public constructor(data: DisclosureData<T>) {
     if (data.length === 2) {
       this.salt = data[0];
       this.value = data[1];
@@ -24,36 +25,31 @@ export class Disclosure {
     throw new SDJWTException('Invalid disclosure data');
   }
 
-  public static fromEncode(s: string) {
-    const item = JSON.parse(Base64Url.decode(s)) as DisclosureData;
-    return Disclosure.fromArray(item);
+  public static fromEncode<T>(s: string) {
+    const item = JSON.parse(Base64Url.decode(s)) as DisclosureData<T>;
+    return Disclosure.fromArray<T>(item);
   }
 
-  public static fromArray(item: DisclosureData) {
+  public static fromArray<T>(item: DisclosureData<T>) {
     return new Disclosure(item);
   }
 
-  public get encoded() {
-    return Base64url.encodeFromJson(this.decoded);
+  public encode() {
+    return Base64Url.encode(JSON.stringify(this.decode()));
   }
 
-  public get decoded(): DisclosureItem {
+  public decode(): DisclosureData<T> {
     return this.key
       ? [this.salt, this.key, this.value]
       : [this.salt, this.value];
   }
 
-  public async digest(hasher: Hasher) {
-    // Memoize value so we don't have to re-compute
+  public digest(hasher: Hasher) {
     if (!this._digest) {
-      const hash = await hasher(this.encoded);
-      this._digest = Base64url.encode(hash);
+      const hash = hasher(this.encode());
+      this._digest = Base64Url.encode(hash);
     }
 
     return this._digest;
-  }
-
-  public toString() {
-    return this.encoded;
   }
 }
